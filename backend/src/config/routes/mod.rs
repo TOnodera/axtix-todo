89ebb83790a5d161fba0b338cc3237c6e7541fc3
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-10-23 14:56:24 +0000

    エラーのベストプラクティスをべた書きで試した。

--- backend/src/config/routes/mod.rs
+++ backend/src/config/routes/mod.rs
@@ -3,5 +3,7 @@ use actix_web::{web, Resource};
 mod todo;
 
 pub fn routes() -> Resource {
-    web::resource("/").route(web::get().to(todo::index))
+    web::resource("/")
+        .route(web::get().to(todo::index))
+        .route(web::post().to(todo::post))
 }
--- backend/src/config/routes/todo.rs
+++ backend/src/config/routes/todo.rs
@@ -1,5 +1,39 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::{HttpResponse, Responder, Result};
 
 pub async fn index() -> impl Responder {
     HttpResponse::Ok().body("hey there")
+}
+
+use actix_web::{
+    error, get,
+    http::{header::ContentType, StatusCode},
+    App, HttpServer,
+};
+use derive_more::{Display, Error};
+
+#[derive(Debug, Display, Error)]
+enum UserError {
+    #[display(fmt = "Validation error on field: {}", field)]
+    ValidationError { field: String },
+}
+
+impl error::ResponseError for UserError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::html())
+            .body(self.to_string())
+    }
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            UserError::ValidationError { .. } => StatusCode::BAD_REQUEST,
+        }
+    }
+}
+
+pub async fn post() -> Result<&'static str> {
+    let result = Err(UserError::ValidationError {
+        field: String::from("test"),
+    });
+
+    Ok(result.map_err(|e| error::ErrorBadRequest(e))?)
 }
