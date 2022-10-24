a964ec0ed551150b16ad85aafb6d6c1179f2d724
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-10-24 12:55:46 +0000

    いったんコミット

--- backend/src/Error/user_error.rs
+++ backend/src/Error/user_error.rs
@@ -1,21 +1,21 @@
+use std::fmt::Display;
+
 use actix_web::{
     error, get,
     http::{header::ContentType, StatusCode},
     App, HttpResponse, HttpServer,
 };
-use derive_more::{Display, Error};
 
-#[derive(Debug, Display, Error)]
+#[derive(Display)]
 pub enum UserError {
-    #[display(fmt = "Validation error on field: {}", field)]
     ValidationError { field: String },
 }
 
 impl error::ResponseError for UserError {
     fn error_response(&self) -> HttpResponse {
         HttpResponse::build(self.status_code())
-            .insert_header(ContentType::html())
-            .body(self.to_string())
+            .insert_header(ContentType::json())
+            .json(self.to_string())
     }
     fn status_code(&self) -> StatusCode {
         match *self {
